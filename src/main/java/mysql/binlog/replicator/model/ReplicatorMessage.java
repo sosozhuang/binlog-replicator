08c40bd27ee1b74196e6e0c89e8fba00c901acf0
ss <sosozhuang@163.com>
2020-10-28 19:13:18 +0800

    Add ReplicatorMessage

---
+++ src/main/java/mysql/binlog/replicator/model/ReplicatorMessage.java
@@ -0,0 +1,51 @@
+package mysql.binlog.replicator.model;
+
+import com.alibaba.otter.canal.protocol.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ * @author zhuangshuo
+ */
+public class ReplicatorMessage {
+    private Message canalMessage;
+    private final List<Event> events;
+    private final List<Future<?>> futures;
+
+    public ReplicatorMessage() {
+        this.events = new ArrayList<>(64);
+        this.futures = new ArrayList<>(64);
+    }
+
+    public Message getCanalMessage() {
+        return canalMessage;
+    }
+
+    public void setCanalMessage(Message canalMessage) {
+        this.canalMessage = canalMessage;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+
+    public List<Future<?>> getFutures() {
+        return futures;
+    }
+
+    public void reset() {
+        canalMessage = null;
+        events.clear();
+        futures.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "ReplicatorMessage{" +
+                ", events=" + events +
+                '}';
+    }
+}
